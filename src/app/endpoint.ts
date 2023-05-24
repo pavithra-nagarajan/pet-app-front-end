f3158d6fc582dcc21a144060b681b54ecc324d1e
pavithra-nagarajan <pavithra.n@revature.com>
2023-05-24 10:35:36 +0000

    commit-1

--- src/app/endpoint.ts
+++ src/app/endpoint.ts
@@ -1,4 +1,4 @@
 // If we define the url in one place and just import it wherever we need it, it will make our lives way easier when/if we have to change it
-export const url = "http://localhost:8080";
+export const url = "https://8080-pavithranag-petappfront-0yx8psx9f77.ws-us97.gitpod.io";
 
 // export const url = "http://petebs-env-1.eba-guijimgt.us-east-1.elasticbeanstalk.com";
